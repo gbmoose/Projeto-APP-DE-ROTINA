3b973d07ba6a905e4926268c2528d8fc1357c710
gbmoose <guibortolan20@gmail.com>
2026-08-26 17:14:13 -0300

    Add color constants to colors.ts

---
+++ src/theme/colors.ts
@@ -0,0 +1,6 @@
+export const colors = {
+  background: '#F6F7FB', surface: '#FFFFFF', primary: '#6757E8', primarySoft: '#EDEAFF',
+  text: '#181A2A', textMuted: '#6D7082', border: '#E6E7EC', success: '#2E9D71',
+  successSoft: '#E2F5ED', warning: '#E29A36', danger: '#D65A67', work: '#4E7DE9',
+  study: '#8E5FDB', training: '#E36D55', personal: '#2E9D71',
+};
